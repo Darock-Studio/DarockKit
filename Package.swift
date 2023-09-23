499717dec7a355e9bbeaee4dec7072f52aa0c904
WindowsMEMZ <3245146430@qq.com>
2023-09-23 14:53:45 +0800

    Updated some files

--- Package.swift
+++ Package.swift
@@ -25,7 +25,7 @@ let package = Package(
             dependencies: [
                 "Alamofire",
                 "SwiftyJSON",
-                "SwiftJWT",
+                //"Swift-JWT",
                 "SwiftDate"
             ]
         )
