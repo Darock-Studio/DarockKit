25a72b37945ec57930cb0169b9b011338668d3e2
WindowsMEMZ <wmemz2333@163.com>
2024-04-13 20:18:19 +0800

    Fixed assess

--- Sources/DarockKit/UIExt.swift
+++ Sources/DarockKit/UIExt.swift
@@ -298,6 +298,10 @@ import WebKit
 public struct WebView: UIViewRepresentable {
     let url: URL
     
+    public init(url: URL) {
+        self.url = url
+    }
+    
     public func makeUIView(context: Context) -> WKWebView {
         let webView = WKWebView()
         webView.load(URLRequest(url: url))
